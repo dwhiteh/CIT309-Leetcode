a36f84500552554a28eafb22e0681663c79e7e98
dwhiteh <97551702+dwhiteh@users.noreply.github.com>
2023-04-05 10:53:15 -0400

    Add files via upload

--- Roman-to-Integer.py
+++ Roman-to-Integer.py
@@ -1,5 +1,5 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
+    def romanToInt(self, s: str = input("Enter a Roman Numeral: ")) -> int:
         roman_num_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
         answer = 0
         current = 0
@@ -12,4 +12,5 @@ class Solution:
                 answer += current
         return answer
 
-
+solution = Solution()
+print("The Roman Numeral you entered converted to an integer is: ",solution.romanToInt(),".")
