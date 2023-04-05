61b5d1e9e0b38106723a7158bd49291bf0494625
dwhiteh <97551702+dwhiteh@users.noreply.github.com>
2023-04-05 12:32:38 -0400

    Add files via upload

--- Roman-to-Integer.py
+++ Roman-to-Integer.py
@@ -1,7 +1,10 @@
 class Solution:
     def romanToInt(self, s: str = input("Enter a Roman Numeral: ")) -> int:
+        #Define Value of Roman Numeral
         roman_num_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        #This will keep track of the integer value of the Roman Numeral
         answer = 0
+        #This is the value of the current roman numeral being converted to integer
         current = 0
         for numeral in s[::-1]:
             previous = current
